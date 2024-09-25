230d4eca1a423a5bcd5deaaed55cdb3b0b7fad11
d1r1 <me@d1r1.me>
2024-09-25 17:28:54 +0200

    finalize encoder trait

--- src/encoder2.rs
+++ src/encoder2.rs
@@ -1,6 +1,6 @@
 use core::marker::PhantomData;
 
-use byteorder::{BigEndian, ByteOrder};
+use byteorder::{BigEndian, ByteOrder, LittleEndian};
 use bytes::{Buf, BytesMut};
 
 use crate::{
@@ -8,7 +8,6 @@ use crate::{
     error::{CodecError, DecodingError},
 };
 
-// pub type SolidityEncoderMode = EncoderMode<BE, 32, true>;
 // pub type WasmEncoderMode = EncoderMode<LE, 4, false>;
 
 pub trait HeaderSized {
@@ -22,28 +21,16 @@ pub trait HeaderSized {
 /// - `ALIGN`: The alignment requirement for the encoded data.
 /// - `SOL_MODE`: A boolean flag indicating whether Solidity-compatible mode is enabled.
 pub trait Encoder<B: ByteOrder, const ALIGN: usize, const SOL_MODE: bool>: Sized {
-    /// Defines the header size for this encoder.
-    ///
-    /// We use an associated type implementing `HeaderSized` instead of a const
-    /// `HEADER_SIZE` because traits with generic associated types (GATs) cannot
-    /// have associated constants that depend on those types.
-    type HeaderSize: HeaderSized;
-
+    const HEADER_SIZE: usize;
     /// Returns the header size for this encoder.
     ///
     /// This method provides a convenient way to access the header size,
     /// which is defined by the associated `HeaderSize` type.
-    fn header_size(&self) -> usize {
-        Self::HeaderSize::HEADER_SIZE
-    }
-
-    /// Calculates the number of bytes needed to encode the value.
     ///
-    /// This includes the header size and any additional space needed for alignment.
-    /// The default implementation aligns the header size to the specified alignment.
-    fn size_hint(&self) -> usize {
-        align_up::<ALIGN>(self.header_size())
-    }
+    /// We use header_size method instead of a const
+    /// `HEADER_SIZE` because traits with generic associated types (GATs) cannot
+    /// have associated constants that depend on those types.
+    // fn header_size() -> usize;
 
     /// Encodes the value into the given buffer at the specified offset.
     ///
@@ -75,16 +62,13 @@ pub trait Encoder<B: ByteOrder, const ALIGN: usize, const SOL_MODE: bool>: Sized
     /// A tuple `(size, data_offset)` where `size` is the total size of the encoded data
     /// and `data_offset` is the offset to the actual data (after the header).
     fn partial_decode(buf: &impl Buf, offset: usize) -> Result<(usize, usize), CodecError>;
-}
 
-pub struct SolidityABI<T>(PhantomData<T>);
-
-impl<T> SolidityABI<T>
-where
-    T: Encoder<BigEndian, 32, true>,
-{
-    pub fn encode(value: &T, buf: &mut BytesMut, offset: usize) -> Result<(), CodecError> {
-        value.encode(buf, offset)
+    /// Calculates the number of bytes needed to encode the value.
+    ///
+    /// This includes the header size and any additional space needed for alignment.
+    /// The default implementation aligns the header size to the specified alignment.
+    fn size_hint(&self) -> usize {
+        align_up::<ALIGN>(Self::HEADER_SIZE)
     }
 }
 
@@ -93,19 +77,51 @@ pub const fn align_up<const ALIGN: usize>(offset: usize) -> usize {
     (offset + ALIGN - 1) & !(ALIGN - 1)
 }
 
-impl HeaderSized for u8 {
-    const HEADER_SIZE: usize = core::mem::size_of::<u8>();
+macro_rules! define_encoder_mode {
+    ($name:ident, $byte_order:ty, $align:expr, $sol_mode:expr) => {
+        pub struct $name<T>(PhantomData<T>);
+
+        impl<T> $name<T>
+        where
+            T: Encoder<$byte_order, $align, $sol_mode>,
+        {
+            pub fn encode(value: &T, buf: &mut BytesMut, offset: usize) -> Result<(), CodecError> {
+                value.encode(buf, offset)
+            }
+
+            pub fn decode(buf: &impl Buf, offset: usize) -> Result<T, CodecError> {
+                T::decode(buf, offset)
+            }
+
+            pub fn partial_decode(
+                buf: &impl Buf,
+                offset: usize,
+            ) -> Result<(usize, usize), CodecError> {
+                T::partial_decode(buf, offset)
+            }
+
+            pub fn size_hint(value: &T) -> usize {
+                value.size_hint()
+            }
+        }
+    };
 }
 
-impl<B: ByteOrder, const ALIGN: usize> Encoder<B, ALIGN, true> for u8 {
-    type HeaderSize = u8;
-    fn size_hint(&self) -> usize {
-        align_up::<ALIGN>(Self::HEADER_SIZE as usize)
-    }
+// Define encoder modes for Solidity and Wasm ABI
+define_encoder_mode!(SolidityABI, BigEndian, 32, true);
+define_encoder_mode!(WasmABI, LittleEndian, 4, false);
 
+// Example of implementing the Encoder trait for u8
+impl<B: ByteOrder, const ALIGN: usize, const SOL_MODE: bool> Encoder<B, ALIGN, SOL_MODE> for u8 {
+    const HEADER_SIZE: usize = core::mem::size_of::<u8>();
+
+    fn size_hint(&self) -> usize {
+        align_up::<ALIGN>(<Self as Encoder<B, ALIGN, SOL_MODE>>::HEADER_SIZE)
+    }
     fn encode(&self, buf: &mut BytesMut, offset: usize) -> Result<(), CodecError> {
         let aligned_offset = align_up::<ALIGN>(offset);
-        let word_size = align_up::<ALIGN>(ALIGN.max(Self::HEADER_SIZE));
+        let word_size =
+            align_up::<ALIGN>(ALIGN.max(<Self as Encoder<B, ALIGN, SOL_MODE>>::HEADER_SIZE));
 
         if buf.len() < aligned_offset + word_size {
             buf.resize(aligned_offset + word_size, 0);
@@ -118,7 +134,8 @@ impl<B: ByteOrder, const ALIGN: usize> Encoder<B, ALIGN, true> for u8 {
 
     fn decode(buf: &impl Buf, offset: usize) -> Result<Self, CodecError> {
         let aligned_offset = align_up::<ALIGN>(offset);
-        let word_size = align_up::<ALIGN>(ALIGN.max(Self::HEADER_SIZE));
+        let word_size =
+            align_up::<ALIGN>(ALIGN.max(<Self as Encoder<B, ALIGN, SOL_MODE>>::HEADER_SIZE));
 
         if buf.remaining() < aligned_offset + word_size {
             return Err(CodecError::Decoding(DecodingError::BufferTooSmall {
@@ -139,19 +156,25 @@ impl<B: ByteOrder, const ALIGN: usize> Encoder<B, ALIGN, true> for u8 {
     }
 
     fn partial_decode(buf: &impl Buf, offset: usize) -> Result<(usize, usize), CodecError> {
-        Ok((0, Self::HEADER_SIZE))
+        Ok((0, <Self as Encoder<B, ALIGN, SOL_MODE>>::HEADER_SIZE))
     }
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
     #[test]
     fn test_align_up() {
         let value: u8 = 0x1;
         let mut buf = BytesMut::new();
-
         SolidityABI::encode(&value, &mut buf, 0).unwrap();
+        let encoded = buf.freeze();
+
+        println!("Encoded: {:?}", encoded);
+
+        let mut buf = BytesMut::new();
+        WasmABI::encode(&value, &mut buf, 0).unwrap();
     }
 }
