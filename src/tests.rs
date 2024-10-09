c3bcd95cdf7865fd9f6e7f99cefbdab8077ff4ed
d1r1 <me@d1r1.me>
2024-10-09 14:37:22 +0200

    prettify tests

--- src/tests.rs
+++ src/tests.rs
@@ -404,63 +404,7 @@ fn test_struct_encoding_sol() {
 }
 
 #[test]
-fn test_solidity_abi_u32_encoding() {
-    let test_value: u32 = 0x12345678;
-
-    let mut buf = BytesMut::new();
-    SolidityABI::<u32>::encode(&test_value, &mut buf, 0).unwrap();
-
-    let encoded = buf.freeze();
-
-    let alloy_value = sol_data::Uint::<32>::abi_encode(&test_value);
-
-    assert_eq!(encoded, alloy_value);
-
-    let decoded = SolidityABI::<u32>::decode(&&alloy_value[..], 0).unwrap();
-
-    assert_eq!(decoded, test_value);
-}
-
-#[test]
-fn test_i32_sol() {
-    let test_value: i32 = -0x12345678;
-    let mut buf = BytesMut::new();
-    SolidityABI::<i32>::encode(&test_value, &mut buf, 0).unwrap();
-    let encoded = buf.freeze();
-
-    let alloy_value = sol_data::Int::<32>::abi_encode(&test_value);
-
-    let cfg = HexConfig {
-        title: false,
-        width: 8,
-        group: 0,
-        ..HexConfig::default()
-    };
-
-    println!(">>>>: {}", config_hex(&encoded, cfg));
-
-    // println!("Encoded i32: {:?}", hex::encode(&encoded));
-
-    assert_eq!(hex::encode(&encoded), hex::encode(&alloy_value));
-}
-
-#[test]
-fn test_u32_sol() {
-    let test_value: u32 = 0x12345678;
-    let mut buf = BytesMut::new();
-    SolidityABI::<u32>::encode(&test_value, &mut buf, 0).unwrap();
-
-    let encoded = buf.freeze();
-
-    let alloy_value = sol_data::Uint::<32>::abi_encode(&test_value);
-
-    println!("Encoded u32: {:?}", hex::encode(&encoded));
-
-    assert_eq!(hex::encode(&encoded), hex::encode(&alloy_value));
-}
-
-#[test]
-fn test_solidity_abi_fixed_array_encoding() {
+fn test_fixed_sol() {
     let test_value: [u32; 3] = [0x11111111, 0x22222222, 0x33333333];
 
     let mut buf = BytesMut::new();
@@ -480,7 +424,7 @@ fn test_solidity_abi_fixed_array_encoding() {
 }
 
 #[test]
-fn test_solidity_abi_bytes_encoding() {
+fn test_bytes_sol() {
     let original = alloy_primitives::Bytes::from_static(b"hello world");
 
     let mut buf = BytesMut::new();
@@ -495,27 +439,27 @@ fn test_solidity_abi_bytes_encoding() {
 
     assert_eq!(encoded.to_vec(), expected);
 
-    // let sol_encoded = sol_data::Bytes::abi_encode(&original);
+    let sol_encoded = sol_data::Bytes::abi_encode(&original);
 
-    // assert_eq!(encoded.to_vec(), sol_encoded);
+    assert_eq!(encoded.to_vec(), sol_encoded);
 
-    // let (offset, length) =
-    //     SolidityABI::<alloy_primitives::Bytes>::partial_decode(&&sol_encoded[..], 0).unwrap();
-    // assert_eq!(offset, 64);
-    // assert_eq!(length, 11);
+    let (offset, length) =
+        SolidityABI::<alloy_primitives::Bytes>::partial_decode(&&sol_encoded[..], 0).unwrap();
+    assert_eq!(offset, 32);
+    assert_eq!(length, 11);
 
-    // let decoded = SolidityABI::<alloy_primitives::Bytes>::decode(&&sol_encoded[..], 0).unwrap();
+    let decoded = SolidityABI::<alloy_primitives::Bytes>::decode(&&sol_encoded[..], 0).unwrap();
 
-    // let alloy_decoded = sol_data::Bytes::abi_decode(&sol_encoded, false).unwrap();
+    let alloy_decoded = sol_data::Bytes::abi_decode(&sol_encoded, false).unwrap();
 
-    // println!("Decoded Bytes (our): {:?}", decoded.to_vec());
-    // println!("Decoded Bytes (alloy): {:?}", alloy_decoded.to_vec());
+    println!("Decoded Bytes (our): {:?}", decoded.to_vec());
+    println!("Decoded Bytes (alloy): {:?}", alloy_decoded.to_vec());
 
-    // assert_eq!(decoded, original);
+    assert_eq!(decoded, original);
 }
 
 #[test]
-fn test_solidity_abi_fixed_bytes_encoding() {
+fn test_fixed_bytes_sol() {
     // Use FixedBytes<11> to match the length of "hello world"
     let original = alloy_primitives::FixedBytes::<11>::from_slice(b"hello world");
 
@@ -551,7 +495,7 @@ fn test_solidity_abi_fixed_bytes_encoding() {
 }
 
 #[test]
-fn test_address_encoding() {
+fn test_address_sol() {
     let original =
         alloy_primitives::Address::from(hex!("f39Fd6e51aad88F6F4ce6aB8827279cffFb92266"));
 
@@ -573,7 +517,7 @@ fn test_address_encoding() {
 }
 
 #[test]
-fn test_vec_solidity_abi_simple() {
+fn test_vec_sol_simple() {
     let original: Vec<u32> = vec![1, 2, 3];
     let mut buf = BytesMut::new();
     SolidityABI::encode(&original, &mut buf, 0).unwrap();
@@ -597,7 +541,7 @@ fn test_vec_solidity_abi_simple() {
 }
 
 #[test]
-fn test_vec_solidity_abi_nested() {
+fn test_vec_sol_nested() {
     let original: Vec<Vec<u32>> = vec![vec![1, 2, 3], vec![4, 5]];
     let mut buf = BytesMut::new();
     SolidityABI::encode(&original, &mut buf, 0).unwrap();
@@ -630,7 +574,7 @@ fn test_vec_solidity_abi_nested() {
 }
 
 #[test]
-fn test_vec_wasm_abi() {
+fn test_vec_wasm_nested() {
     let original: Vec<Vec<u32>> = vec![vec![1u32, 2, 3], vec![4, 5], vec![6, 7, 8, 9, 10]];
     let mut buf = BytesMut::new();
     WasmABI::<Vec<Vec<u32>>>::encode(&original, &mut buf, 0).unwrap();
@@ -648,7 +592,7 @@ fn test_vec_wasm_abi() {
 }
 
 #[test]
-fn test_empty_vector() {
+fn test_vec_empty_sol() {
     let empty_vec: Vec<u32> = vec![];
 
     let mut buf = BytesMut::new();
@@ -663,7 +607,7 @@ fn test_empty_vector() {
     assert_eq!(decoded, empty_vec);
 }
 #[test]
-fn test_empty_bytes_solidity() {
+fn test_bytes_empty_sol() {
     let original: alloy_primitives::Bytes = alloy_primitives::Bytes::new();
     let mut buf = BytesMut::new();
 
@@ -683,7 +627,7 @@ fn test_empty_bytes_solidity() {
 }
 
 #[test]
-fn test_vec_partial_decoding_solidity() {
+fn test_vec_partial_decoding_sol() {
     let original: Vec<u32> = vec![1u32, 2, 3, 4, 5];
     let mut buf = BytesMut::new();
     SolidityABI::<Vec<u32>>::encode(&original, &mut buf, 0).unwrap();
@@ -717,7 +661,7 @@ fn test_vec_partial_decoding_wasm() {
 }
 
 #[test]
-fn test_simple_map_sol() {
+fn test_map_sol_simple() {
     let mut original = HashMap::new();
     original.insert(10, 20);
     original.insert(1, 5);
@@ -742,89 +686,9 @@ fn test_simple_map_sol() {
     assert_eq!(decoded, original);
 }
 #[test]
-fn test_simple_map_sol_abi_tmp() {
-    let mut original = HashMap::new();
-    original.insert(5, 6);
-
-    let mut buf = BytesMut::new();
-    SolidityABI::encode(&original, &mut buf, 0).unwrap();
-
-    let encoded = buf.freeze();
-    println!("Encoded Map: {:?}", hex::encode(&encoded));
-
-    // let expected_encoded = hex!(
-    //     "000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000064000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000000000000000000000000000000000050000000000000000000000000000000000000000000000000000000000000014000000000000000000000000000000000000000000000000000000000000003c"
-    // );
-    // 0   - 0000000000000000000000000000000000000000000000000000000000000020 - Смещение до начала
-    // данных (32) 32  - 0000000000000000000000000000000000000000000000000000000000000003 -
-    // Длина HashMap (3 элемента)
-    // 64  - 0000000000000000000000000000000000000000000000000000000000000040 - Смещение до начала
-    // ключей (64) 96  - 0000000000000000000000000000000000000000000000000000000000000100 -
-    // Смещение до начала значений (256)
-
-    // Ключи:
-    // 128 - 0000000000000000000000000000000000000000000000000000000000000003 - Длина массива ключей
-    // (3) 160 - 0000000000000000000000000000000000000000000000000000000000000001 - Ключ 1 (1)
-    // 192 - 000000000000000000000000000000000000000000000000000000000000000a - Ключ 2 (10)
-    // 224 - 0000000000000000000000000000000000000000000000000000000000000064 - Ключ 3 (100)
-
-    // Значения:
-    // 256 - 0000000000000000000000000000000000000000000000000000000000000003 - Длина массива
-    // значений (3) 288 - 0000000000000000000000000000000000000000000000000000000000000005 -
-    // Значение 1 (5) 320 - 0000000000000000000000000000000000000000000000000000000000000014 -
-    // Значение 2 (20) 352 - 000000000000000000000000000000000000000000000000000000000000003c -
-    // Значение 3 (60)
-
-    // assert_eq!(encoded.to_vec(), expected_encoded);
-
-    // println!("Encoded Map: {:?}", hex::encode(&encoded));
-
-    // let decoded = SolidityABI::<HashMap<u32, u32>>::decode(&&encoded[..], 0).unwrap();
-
-    // assert_eq!(decoded, original);
-}
-
-// Outer HashMap header
-// 0   - 00000000000000000000000000000000000000000000000000000000000000020 // Offset to data (32)
-// 32  - 00000000000000000000000000000000000000000000000000000000000000002 // Number of elements (2)
-// 64  - 0000000000000000000000000000000000000000000000000000000000000040 // Offset to keys (64)
-// 96  - 00000000000000000000000000000000000000000000000000000000000000e0 // Offset to values (224)
-
-// // Outer HashMap keys
-// 128 - 0000000000000000000000000000000000000000000000000000000000000002 // Number of keys (2)
-// 160 - 0000000000000000000000000000000000000000000000000000000000000001 // Key 1 (1)
-// 192 - 0000000000000000000000000000000000000000000000000000000000000002 // Key 2 (2)
-
-// // Outer HashMap values (offsets to inner HashMaps)
-// 224 - 0000000000000000000000000000000000000000000000000000000000000002 // Number of values (2)
-// 256 - 0000000000000000000000000000000000000000000000000000000000000100 // Offset to first inner
-// HashMap (256) 288 - 0000000000000000000000000000000000000000000000000000000000000180 // Offset to
-// second inner HashMap (384)
-
-// // First inner HashMap (for key 1)
-// 320 - 0000000000000000000000000000000000000000000000000000000000000020 // Offset to data (32)
-// 352 - 0000000000000000000000000000000000000000000000000000000000000001 // Number of elements (1)
-// 384 - 0000000000000000000000000000000000000000000000000000000000000040 // Offset to keys (64)
-// 416 - 0000000000000000000000000000000000000000000000000000000000000080 // Offset to values (128)
-// 448 - 0000000000000000000000000000000000000000000000000000000000000001 // Number of keys (1)
-// 480 - 0000000000000000000000000000000000000000000000000000000000000005 // Key (5)
-// 512 - 0000000000000000000000000000000000000000000000000000000000000001 // Number of values (1)
-// 544 - 0000000000000000000000000000000000000000000000000000000000000006 // Value (6)
-
-// // Second inner HashMap (for key 2)
-// 576 - 0000000000000000000000000000000000000000000000000000000000000020 // Offset to data (32)
-// 608 - 0000000000000000000000000000000000000000000000000000000000000001 // Number of elements (1)
-// 640 - 0000000000000000000000000000000000000000000000000000000000000040 // Offset to keys (64)
-// 672 - 0000000000000000000000000000000000000000000000000000000000000080 // Offset to values (128)
-// 704 - 0000000000000000000000000000000000000000000000000000000000000001 // Number of keys (1)
-// 736 - 0000000000000000000000000000000000000000000000000000000000000007 // Key (7)
-// 768 - 0000000000000000000000000000000000000000000000000000000000000001 // Number of values (1)
-// 800 - 0000000000000000000000000000000000000000000000000000000000000008 // Value (8)
-#[test]
-fn test_nested_map_sol_abi1() {
+fn test_map_sol_nested() {
     let mut original = HashMap::new();
     original.insert(9, HashMap::from([(8, 7)]));
-    // original.insert(2, HashMap::from([(7, 8)]));
 
     println!("Original Map: {:?}", original);
 
@@ -845,7 +709,7 @@ fn test_nested_map_sol_abi1() {
     assert_eq!(decoded, original);
 }
 #[test]
-fn test_nested_map_sol_abi2() {
+fn test_map_sol_nested_2() {
     let mut original = HashMap::new();
     original.insert(1, HashMap::from([(5, 6)]));
     original.insert(2, HashMap::from([(7, 8)]));
@@ -870,7 +734,7 @@ fn test_nested_map_sol_abi2() {
 }
 
 #[test]
-fn test_simple_map_wasm_abi() {
+fn test_map_wasm_simple() {
     let mut original = HashMap::new();
     original.insert(100, 20);
     original.insert(3, 5);
@@ -895,7 +759,7 @@ fn test_simple_map_wasm_abi() {
 }
 
 #[test]
-fn test_nested_map_wasm_abi() {
+fn test_map_wasm_nested() {
     let mut original = HashMap::new();
     original.insert(1, HashMap::from([(5, 6)]));
     original.insert(2, HashMap::from([(7, 8)]));
