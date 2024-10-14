386f624edb1b45b86026ae8afe795ff336f68c8f
d1r1 <me@d1r1.me>
2024-10-14 14:48:39 +0200

    export derive from codec lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![allow(unused_imports)]
 extern crate alloc;
 
 pub mod bytes;
@@ -12,3 +14,9 @@ pub mod vec;
 
 #[cfg(test)]
 mod tests;
+
+#[cfg(feature = "derive")]
+extern crate codec_derive;
+
+#[cfg(feature = "derive")]
+pub use codec_derive::Codec;
